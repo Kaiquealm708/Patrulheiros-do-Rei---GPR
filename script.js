93dd30d906ad09f444dcf08c0f7da4cf53f0e04c
Kaique <kaique.tubarao2017@gmail.com>
2026-04-03 13:08:44 -0300

    parallax & blur

--- script.js
+++ script.js
@@ -29,3 +29,19 @@ slides.style.transform = `translateX(-${index * 100}%)`
 function toggleMenu() {
   document.querySelector(".menu").classList.toggle("active");
 }
+
+window.addEventListener("scroll", function () {
+    const section = document.querySelector(".vaquinha");
+    const bg = document.querySelector(".vaquinha-bg");
+
+    const rect = section.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+
+    // só ativa quando a section estiver visível
+    if (rect.top < windowHeight && rect.bottom > 0) {
+      let scrollPercent = (windowHeight - rect.top) / (windowHeight + rect.height);
+
+      bg.style.transform =
+        "translateY(" + (scrollPercent * 100) + "px) scale(1.1)";
+    }
+  });
